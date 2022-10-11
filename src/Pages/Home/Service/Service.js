34f4e68d5687a7ed1a795a4e73aef698423b581c
Abadur Rahman Imtiaz <83642378+arimtiaz@users.noreply.github.com>
2022-10-11 15:17:17 +0600

    Working On Responisveness

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import './Service.css'
 
 const Service = ({service}) => {
     const {name, img, description, price} = service;
 
     return (
-        <div>
+        <div className='service'>
             <img src={img} alt="" />
             <h2>Service: {name}</h2>
+            <p>{description}</p>
         </div>
     );
 };
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
-
+import './Services.css'
 const Services = () => {
     const [services, setServices] = useState([]);
 
@@ -12,7 +12,10 @@ const Services = () => {
 
     return (
         <div>
-            <h1>Services: {services.length}</h1>
+            <div className="services-title">
+                <h1>Services</h1>
+            </div>
+            <div className='services-container'>
             {
                 services.map(service => <Service
                 key={service.id}
@@ -20,6 +23,7 @@ const Services = () => {
                 >
                 </Service>)
             }
+        </div>
         </div>
     );
 };
